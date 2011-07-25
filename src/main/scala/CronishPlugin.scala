3c2970ad43952eb35f9678c715c707d2b908b019
Philip Cali <philip.cali@gmail.com>
2011-07-24 21:17:03 -0500

    Small refactorings.

--- src/main/scala/CronishPlugin.scala
+++ src/main/scala/CronishPlugin.scala
@@ -5,18 +5,6 @@ import complete.DefaultParsers._
 
 import com.github.philcali.cronish.dsl._
 
-object CronishKeys {
-  val Cronish = config("cronish") 
-
-  val tasks = SettingKey[Seq[Scheduled]]("tasks", "Actively defined crons.")
-  val list = TaskKey[Unit]("list", "Lists all the active tasks")
-
-  val addSh = InputKey[Unit]("add-sh", 
-              "Adds a cronish task that executes a system command.")
-  val addSbt = InputKey[Unit]("add-sbt",
-              "Adds a sbt task to be executed at a defined interval.")
-}
-
 object CronishPlugin extends Plugin {
   import CronishKeys._
 
